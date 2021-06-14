55234c9248eda1f1b1c941feaee7f4cc6832f6c9
rusairdrop <rusairdrop@gmail.com>
2021-06-14 19:12:21 +0300

    remade jquery to js

--- script/main.js
+++ script/main.js
@@ -1,19 +1,18 @@
 /* Табы Dashboard, Wallet, Rates
 ==================================*/
-
 const tabTrigger = document.querySelectorAll('.js-tab-trigger');
 const tabContent = document.querySelectorAll('.js-tab-content');
 
 for (let i = 0; i < tabTrigger.length; i++) {
   tabTrigger[i].onclick = function () {
-    for (let k = 0; k <tabTrigger.length; k++){
+    for (let k = 0; k < tabTrigger.length; k++) {
       tabTrigger[k].classList.remove('active');
     }
     this.classList.add('active');
 
     let tabTriggerAttr = this.getAttribute('data-tab');
 
-    for (let j =0; j < tabContent.length; j++) {
+    for (let j = 0; j < tabContent.length; j++) {
       tabContent[j].classList.remove('active');
       let tabContentAttr = tabContent[j].getAttribute('data-tab');
       if (tabContentAttr === tabTriggerAttr) {
@@ -25,18 +24,26 @@ for (let i = 0; i < tabTrigger.length; i++) {
 
 /* Бургер меню
 ==================================*/
-$(document).ready(function () {
-  $(".header__burger").click(function (e) {
-    $(".header__burger,.header__burger-menu, .burger-overlay").toggleClass(
-      "active"
-    );
-  });
-  $(".burger-overlay").click(function (e) {
-    $(
-      ".header__burger,.header__burger-menu, .burger-overlay, .ham, .hamRotate, .ham4"
-    ).removeClass("active");
-  });
-});
+const burger = document.querySelector('.header__burger');
+const burgerOverlay = document.querySelector('.burger-overlay');
+const burgerMenu = document.querySelector('.header__burger-menu');
+const burgerSvg = document.querySelector('.ham, .hamRotate, .ham4');
+
+burger.onclick = function () {
+  burger.classList.toggle('active');
+  burgerOverlay.classList.toggle('active');
+  burgerMenu.classList.toggle('active');
+  for (let i = 0; i < burgerSvg.length; i++) {
+    burgerSvg[i].classList.toggle('active');
+  }
+}
+burgerOverlay.onclick = function () {
+  burger.classList.remove('active');
+  burgerOverlay.classList.remove('active');
+  burgerMenu.classList.remove('active');
+  burgerSvg.classList.remove('active');
+}
+
 
 /* Смена темы оформления
 ==================================*/
