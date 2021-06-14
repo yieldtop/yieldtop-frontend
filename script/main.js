b290abfc56a84744f98508846b3b745aa0d15a85
rusairdrop <rusairdrop@gmail.com>
2021-06-14 16:37:38 +0300

    new design, js learning

--- script/main.js
+++ script/main.js
@@ -1,31 +1,85 @@
-// табы dashboard и wallet
-$('.js-tab-trigger').click(function() {
-  let id = $(this).attr('data-tab'),
-      content = $('.js-tab-content[data-tab="'+ id +'"]');
+/* Табы Dashboard, Wallet, Rates
+==================================*/
 
-$('.js-tab-trigger.active').removeClass('active');
-$(this).addClass('active');
+const tabTrigger = document.querySelectorAll('.js-tab-trigger');
+const tabContent = document.querySelectorAll('.js-tab-content');
 
-$('.js-tab-content.active').removeClass('active');
-  content.addClass('active');
-});
+for (let i = 0; i < tabTrigger.length; i++) {
+  tabTrigger[i].onclick = function () {
+    for (let k = 0; k <tabTrigger.length; k++){
+      tabTrigger[k].classList.remove('active');
+    }
+    this.classList.add('active');
 
-$('.js-tab-trigger').click(function() {
-  let id = $(this).attr('data-tab'),
-      content = $('.js-tab-content[data-tab="'+ id +'"]');
-});
+    let tabTriggerAttr = this.getAttribute('data-tab');
 
-// бургер меню
-$(document).ready(function() {
-  $('.header__burger').click(function(e) {
-    $('.header__burger,.header__burger-menu').toggleClass('active');
-  });
-  $('.header__list').click(function(e) {
-      $('.header__burger,.header__burger-menu').removeClass('active');
-  });
-});
-
-// смена темы оформления
-theme.onclick = function() {
-  document.body.classList.toggle('dark-mode');
+    for (let j =0; j < tabContent.length; j++) {
+      tabContent[j].classList.remove('active');
+      let tabContentAttr = tabContent[j].getAttribute('data-tab');
+      if (tabContentAttr === tabTriggerAttr) {
+        tabContent[j].classList.add('active');
+      }
+    }
+  }
 }
+
+/* Бургер меню
+==================================*/
+$(document).ready(function () {
+  $(".header__burger").click(function (e) {
+    $(".header__burger,.header__burger-menu, .burger-overlay").toggleClass(
+      "active"
+    );
+  });
+  $(".burger-overlay").click(function (e) {
+    $(
+      ".header__burger,.header__burger-menu, .burger-overlay, .ham, .hamRotate, .ham4"
+    ).removeClass("active");
+  });
+});
+
+/* Смена темы оформления
+==================================*/
+document.querySelector(".themetoggle").addEventListener("click", (event) => {
+  event.preventDefault();
+  if (localStorage.getItem("theme") === "light") {
+    localStorage.removeItem("theme");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+  addLightClassToBody();
+});
+
+function addLightClassToBody() {
+  try {
+    if (localStorage.getItem("theme") === "light") {
+      document.querySelector("body").classList.add("light");
+      document.querySelector(".themetoggle span").textContent = "brightness_5";
+    } else {
+      document.querySelector("body").classList.remove("light");
+      document.querySelector(".themetoggle span").textContent = "brightness_4";
+    }
+  } catch (err) {
+  }
+}
+
+addLightClassToBody();
+
+/* Модальное окно Connect Wallet
+==================================*/
+const btnConnect = document.getElementById("btn-connect"),
+  modal = document.getElementById("wrapper-modal"),
+  btnClose = document.getElementById("btn-close"),
+  overlay = document.getElementById("overlay");
+
+btnConnect.addEventListener("click", (addClass = () => {
+    modal.classList.add("active");
+  })
+);
+
+const delClass = () => {
+  modal.classList.remove("active");
+};
+
+btnClose.addEventListener("click", delClass);
+overlay.addEventListener("click", delClass);
